19b22ea474eb5bd0e31155a705995f032c76e9f1
ghypnus <ghypnus@163.com>
2020-10-17 21:37:14 +0800

    工具栏增加样式参数

--- src/toolbar/Toolbar.js
+++ src/toolbar/Toolbar.js
@@ -7,13 +7,13 @@ import React from 'react';
 import classnames from 'classnames';
 
 const Toolbar = (data) => {
-  const { prefixCls, align = 'right', children } = data;
+  const { prefixCls, style, align = 'right', children } = data;
   const wrapCls = classnames(prefixCls, {
     [`${prefixCls}-align-${align}`]: align,
   });
 
   return (
-    <div className={wrapCls}>
+    <div className={wrapCls} style={style}>
       <div className={`${prefixCls}-wrap`}>
         {children}
       </div>
